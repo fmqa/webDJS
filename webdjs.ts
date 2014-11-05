b05991f1392325441f4b6e28a77a79447c5d42cb
Fadi Moukayed <smfadi@gmail.com>
2014-11-05 19:33:43 +0100

    Rename RGB control method

--- webdjs.ts
+++ webdjs.ts
@@ -1,5 +1,5 @@
 module WebDJS {
-	export module VJ {
+    export module VJ {
 	    /**
 	     * Generic OpenGL operation.
 	     */
@@ -96,46 +96,46 @@ module WebDJS {
 		 * Simple Renderer - Render HTML Image/Video to GL context.
 		 */
         export class Simple implements GLOp {
-		    private texOp : GLOp;
-		    private vertexShader : WebGLShader;
-		    private fragmentShader : WebGLShader;
-		    private shaderProgram : WebGLProgram;
-		    private texture : WebGLTexture;
-		    private xyAttribLocation : number;
-		    private samplerAttribLocation : WebGLUniformLocation;
-		    private rgbaAttribLocation : WebGLUniformLocation;
-		    private vertexBuffer : WebGLBuffer;
-		    private indexBuffer : WebGLBuffer;
-		    private vertexArray : RectangularVertexArrayOp;
-		    private rgba : Float32Array = new Float32Array([1,1,1,1]);
-		    private initialized : boolean = false;
-		    private dirty : boolean = false;
-		    private recolour : boolean = false;
-		    image(img : HTMLImageElement) : void {
-		        if (!this.initialized) {
-		            return;
-		        }
-		        this.texOp = new HTMLImageTexOp(img, this.texture);
-		    }
-		    video(vid : HTMLVideoElement) : void {
-		        if (!this.initialized) {
-		            return;
-		        }
-		        this.texOp = new HTMLVideoElementTexOp(vid, this.texture);
-		    }
-		    initialize(gl : WebGLRenderingContext) : void {
-		        if (this.initialized) {
-		            return;
-		        }
-		        
+            private texOp : GLOp;
+            private vertexShader : WebGLShader;
+            private fragmentShader : WebGLShader;
+            private shaderProgram : WebGLProgram;
+            private texture : WebGLTexture;
+            private xyAttribLocation : number;
+            private samplerAttribLocation : WebGLUniformLocation;
+            private rgbaAttribLocation : WebGLUniformLocation;
+            private vertexBuffer : WebGLBuffer;
+            private indexBuffer : WebGLBuffer;
+            private vertexArray : RectangularVertexArrayOp;
+            private rgba : Float32Array = new Float32Array([1,1,1,1]);
+            private initialized : boolean = false;
+            private dirty : boolean = false;
+            private recolor : boolean = false;
+            image(img : HTMLImageElement) : void {
+                if (!this.initialized) {
+                    return;
+                }
+                this.texOp = new HTMLImageTexOp(img, this.texture);
+            }
+            video(vid : HTMLVideoElement) : void {
+                if (!this.initialized) {
+                    return;
+                }
+                this.texOp = new HTMLVideoElementTexOp(vid, this.texture);
+            }
+            initialize(gl : WebGLRenderingContext) : void {
+                if (this.initialized) {
+                    return;
+                }
+                
                 this.vertexShader = gl.createShader(gl.VERTEX_SHADER);
                 gl.shaderSource(this.vertexShader, 
-		            "attribute vec2 vxy;" +
-		            "varying vec2 txy;" +
-		            "void main() {" +
-		            "   gl_Position = vec4(vxy.x*2.0-1.0, 1.0-vxy.y*2.0, 0, 1);" +
-		            "   txy = vxy;" +
-		            "}");
+                    "attribute vec2 vxy;" +
+                    "varying vec2 txy;" +
+                    "void main() {" +
+                    "   gl_Position = vec4(vxy.x*2.0-1.0, 1.0-vxy.y*2.0, 0, 1);" +
+                    "   txy = vxy;" +
+                    "}");
                 gl.compileShader(this.vertexShader);
 
                 this.fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -180,48 +180,48 @@ module WebDJS {
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
                 
                 this.initialized = true;
-		    }
-		    translate(x : number, y : number) : void {
-		        this.vertexArray.translate(x, y);
-		        this.dirty = true;
-		    }
-		    resize(width : number, height : number) : void {
-		        this.vertexArray.resize(width, height);
-		        this.dirty = true;
-		    }
-		    scaleRgba(r : number, g : number, b : number, a : number) : void {
-		        this.rgba[0] = r;
-		        this.rgba[1] = g;
-		        this.rgba[2] = b;
-		        this.rgba[3] = a;
-		        this.recolour = true;
-		    }
-		    apply(gl : WebGLRenderingContext) : void {
-		        if (!this.initialized) {
-		            return;
-		        }
-		        
-		        gl.useProgram(this.shaderProgram);
-		        
-		        this.texOp.apply(gl);
-		        
-		        if (this.dirty) {
-		            this.vertexArray.apply(gl);
-		            this.dirty = false;
-		        }
-		        
-		        if (this.recolour) {
-		            gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
-		            this.recolour = false;
-		        }
-		        
-		        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-		        gl.vertexAttribPointer(this.xyAttribLocation, 2, gl.FLOAT, false, 0, 0);
-		        
-		        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-		        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-		    }
-		}
-	}
+            }
+            translate(x : number, y : number) : void {
+                this.vertexArray.translate(x, y);
+                this.dirty = true;
+            }
+            resize(width : number, height : number) : void {
+                this.vertexArray.resize(width, height);
+                this.dirty = true;
+            }
+            color(r : number, g : number, b : number, a : number) : void {
+                this.rgba[0] = r;
+                this.rgba[1] = g;
+                this.rgba[2] = b;
+                this.rgba[3] = a;
+                this.recolor = true;
+            }
+            apply(gl : WebGLRenderingContext) : void {
+                if (!this.initialized) {
+                    return;
+                }
+                
+                gl.useProgram(this.shaderProgram);
+                
+                this.texOp.apply(gl);
+                
+                if (this.dirty) {
+                    this.vertexArray.apply(gl);
+                    this.dirty = false;
+                }
+                
+                if (this.recolor) {
+                    gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
+                    this.recolor = false;
+                }
+                
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+                gl.vertexAttribPointer(this.xyAttribLocation, 2, gl.FLOAT, false, 0, 0);
+                
+                gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+                gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+            }
+        }
+    }
 }
 
