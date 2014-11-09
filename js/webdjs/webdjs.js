230ba63fac244f57ef2c27c08e89fa97f816456f
Fadi Moukayed <smfadi@gmail.com>
2014-11-09 18:08:22 +0100

    Simplify initialization logic and use expressive names for some interfaces

--- js/webdjs/webdjs.js
+++ js/webdjs/webdjs.js
@@ -5,148 +5,134 @@ var WebDJS;
         /**
          * HTML Image -> Texture operation.
          */
-        var HTMLImageTexOp = (function () {
-            function HTMLImageTexOp(source, texture) {
+        var GLImageInput = (function () {
+            function GLImageInput(source, texture) {
                 if (texture === void 0) { texture = null; }
                 this.source = source;
                 this.bind(texture);
             }
-            HTMLImageTexOp.prototype.bind = function (texture) {
+            GLImageInput.prototype.bind = function (texture) {
                 this.texture = texture;
+                this.reload();
             };
-            HTMLImageTexOp.prototype.apply = function (gl) {
+            GLImageInput.prototype.reload = function () {
+                this.loaded = false;
+            };
+            GLImageInput.prototype.apply = function (gl) {
                 gl.activeTexture(gl.TEXTURE0);
                 gl.bindTexture(gl.TEXTURE_2D, this.texture);
-                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+                if (!this.loaded) {
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+                    this.loaded = true;
+                }
             };
-            return HTMLImageTexOp;
+            return GLImageInput;
         })();
-        VJ.HTMLImageTexOp = HTMLImageTexOp;
+        VJ.GLImageInput = GLImageInput;
         /**
          * HTML Video -> Texture operation.
          */
-        var HTMLVideoElementTexOp = (function () {
-            function HTMLVideoElementTexOp(source, texture) {
+        var GLVideoInput = (function () {
+            function GLVideoInput(source, texture) {
                 if (texture === void 0) { texture = null; }
                 this.source = source;
                 this.bind(texture);
             }
-            HTMLVideoElementTexOp.prototype.bind = function (texture) {
+            GLVideoInput.prototype.bind = function (texture) {
                 this.texture = texture;
+                this.reload();
             };
-            HTMLVideoElementTexOp.prototype.apply = function (gl) {
+            GLVideoInput.prototype.reload = function () {
+                this.loaded = false;
+            };
+            GLVideoInput.prototype.apply = function (gl) {
                 gl.activeTexture(gl.TEXTURE0);
                 gl.bindTexture(gl.TEXTURE_2D, this.texture);
-                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+                if (!this.loaded) {
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+                    this.loaded = true;
+                }
             };
-            return HTMLVideoElementTexOp;
+            return GLVideoInput;
         })();
-        VJ.HTMLVideoElementTexOp = HTMLVideoElementTexOp;
+        VJ.GLVideoInput = GLVideoInput;
         /**
          * Vertex Array Object.
          *
          * Transforms <x,y,width,height>-Tuples to a pair of Vertex/Index Buffers.
          */
-        var RectangularVertexArrayOp = (function () {
-            function RectangularVertexArrayOp(vertexBuffer, indexBuffer) {
+        var GLRectangleVertexArray = (function () {
+            function GLRectangleVertexArray(vertexBuffer, indexBuffer) {
                 if (vertexBuffer === void 0) { vertexBuffer = null; }
                 if (indexBuffer === void 0) { indexBuffer = null; }
                 this.vertices = new Float32Array(8);
                 this.indices = new Uint16Array([0, 1, 2, 0, 2, 3]);
+                this.loaded = false;
                 this.bind(vertexBuffer, indexBuffer);
                 this.translate(0, 0);
                 this.resize(1, 1);
             }
-            RectangularVertexArrayOp.prototype.bind = function (vertexBuffer, indexBuffer) {
+            GLRectangleVertexArray.prototype.bind = function (vertexBuffer, indexBuffer) {
                 this.vertexBuffer = vertexBuffer;
                 this.indexBuffer = indexBuffer;
+                this.changed = true;
+                this.loaded = false;
             };
-            RectangularVertexArrayOp.prototype.translate = function (x, y) {
+            GLRectangleVertexArray.prototype.translate = function (x, y) {
                 this.x = x;
                 this.y = y;
                 this.vertices[0] = this.x;
                 this.vertices[1] = this.y;
                 this.vertices[3] = this.y;
                 this.vertices[6] = this.x;
+                this.changed = true;
             };
-            RectangularVertexArrayOp.prototype.resize = function (width, height) {
+            GLRectangleVertexArray.prototype.resize = function (width, height) {
                 this.width = width;
                 this.height = height;
                 this.vertices[2] = this.x + this.width;
                 this.vertices[4] = this.x + this.width;
                 this.vertices[5] = this.y + this.height;
                 this.vertices[7] = this.y + this.height;
+                this.changed = true;
             };
-            RectangularVertexArrayOp.prototype.apply = function (gl) {
+            GLRectangleVertexArray.prototype.apply = function (gl) {
                 gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-                gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+                if (this.changed) {
+                    gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+                    this.changed = false;
+                }
                 gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-                gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+                if (!this.loaded) {
+                    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+                    this.loaded = true;
+                }
             };
-            return RectangularVertexArrayOp;
+            return GLRectangleVertexArray;
         })();
-        VJ.RectangularVertexArrayOp = RectangularVertexArrayOp;
+        VJ.GLRectangleVertexArray = GLRectangleVertexArray;
         /**
          * Simple Renderer - Render HTML Image/Video to GL context.
          */
         var Simple = (function () {
-            function Simple() {
+            function Simple(texOp) {
+                if (texOp === void 0) { texOp = null; }
+                this.vertexArray = new GLRectangleVertexArray();
                 this.rgba = new Float32Array([1, 1, 1, 1]);
                 this.initialized = false;
-                this.dirty = true;
                 this.recolor = true;
+                this.rebind = false;
+                this.texture(texOp);
             }
-            Simple.prototype.image = function (img) {
-                if (!this.initialized) {
-                    return;
-                }
-                this.texOp = new HTMLImageTexOp(img, this.texture);
-            };
-            Simple.prototype.video = function (vid) {
-                if (!this.initialized) {
-                    return;
-                }
-                this.texOp = new HTMLVideoElementTexOp(vid, this.texture);
-            };
-            Simple.prototype.initialize = function (gl) {
-                if (this.initialized) {
-                    return;
-                }
-                this.vertexShader = gl.createShader(gl.VERTEX_SHADER);
-                gl.shaderSource(this.vertexShader, "attribute vec2 vxy;" + "varying vec2 txy;" + "void main() {" + "   gl_Position = vec4(vxy.x*2.0-1.0, 1.0-vxy.y*2.0, 0, 1);" + "   txy = vxy;" + "}");
-                gl.compileShader(this.vertexShader);
-                this.fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-                gl.shaderSource(this.fragmentShader, "precision mediump float;" + "uniform sampler2D sampler;" + "uniform vec4 rgba;" + "varying vec2 txy;" + "void main() {" + "   vec4 texColor = texture2D(sampler, txy);" + "   gl_FragColor = texColor * rgba;" + "}");
-                gl.compileShader(this.fragmentShader);
-                this.shaderProgram = gl.createProgram();
-                gl.attachShader(this.shaderProgram, this.vertexShader);
-                gl.attachShader(this.shaderProgram, this.fragmentShader);
-                gl.linkProgram(this.shaderProgram);
-                gl.useProgram(this.shaderProgram);
-                this.xyAttribLocation = gl.getAttribLocation(this.shaderProgram, "vxy");
-                gl.enableVertexAttribArray(this.xyAttribLocation);
-                this.samplerAttribLocation = gl.getUniformLocation(this.shaderProgram, "sampler");
-                gl.uniform1i(this.samplerAttribLocation, 0);
-                this.rgbaAttribLocation = gl.getUniformLocation(this.shaderProgram, "rgba");
-                gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
-                this.vertexBuffer = gl.createBuffer();
-                this.indexBuffer = gl.createBuffer();
-                this.vertexArray = new RectangularVertexArrayOp(this.vertexBuffer, this.indexBuffer);
-                this.texture = gl.createTexture();
-                gl.bindTexture(gl.TEXTURE_2D, this.texture);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                this.initialized = true;
+            Simple.prototype.texture = function (texOp) {
+                this.texOp = texOp;
+                this.rebind = true;
             };
             Simple.prototype.translate = function (x, y) {
                 this.vertexArray.translate(x, y);
-                this.dirty = true;
             };
             Simple.prototype.resize = function (width, height) {
                 this.vertexArray.resize(width, height);
-                this.dirty = true;
             };
             Simple.prototype.color = function (r, g, b, a) {
                 this.rgba[0] = r;
@@ -157,14 +143,41 @@ var WebDJS;
             };
             Simple.prototype.apply = function (gl) {
                 if (!this.initialized) {
-                    return;
+                    this.vertexShader = gl.createShader(gl.VERTEX_SHADER);
+                    gl.shaderSource(this.vertexShader, "attribute vec2 vxy;" + "varying vec2 txy;" + "void main() {" + "   gl_Position = vec4(vxy.x*2.0-1.0, 1.0-vxy.y*2.0, 0, 1);" + "   txy = vxy;" + "}");
+                    gl.compileShader(this.vertexShader);
+                    this.fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+                    gl.shaderSource(this.fragmentShader, "precision mediump float;" + "uniform sampler2D sampler;" + "uniform vec4 rgba;" + "varying vec2 txy;" + "void main() {" + "   vec4 texColor = texture2D(sampler, txy);" + "   gl_FragColor = texColor * rgba;" + "}");
+                    gl.compileShader(this.fragmentShader);
+                    this.shaderProgram = gl.createProgram();
+                    gl.attachShader(this.shaderProgram, this.vertexShader);
+                    gl.attachShader(this.shaderProgram, this.fragmentShader);
+                    gl.linkProgram(this.shaderProgram);
+                    gl.useProgram(this.shaderProgram);
+                    this.xyAttribLocation = gl.getAttribLocation(this.shaderProgram, "vxy");
+                    gl.enableVertexAttribArray(this.xyAttribLocation);
+                    this.samplerAttribLocation = gl.getUniformLocation(this.shaderProgram, "sampler");
+                    gl.uniform1i(this.samplerAttribLocation, 0);
+                    this.rgbaAttribLocation = gl.getUniformLocation(this.shaderProgram, "rgba");
+                    gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
+                    this.vertexBuffer = gl.createBuffer();
+                    this.indexBuffer = gl.createBuffer();
+                    this.vertexArray.bind(this.vertexBuffer, this.indexBuffer);
+                    this.glTexture = gl.createTexture();
+                    gl.bindTexture(gl.TEXTURE_2D, this.glTexture);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                    this.initialized = true;
+                }
+                if (this.rebind) {
+                    this.texOp.bind(this.glTexture);
+                    this.rebind = false;
                 }
                 gl.useProgram(this.shaderProgram);
                 this.texOp.apply(gl);
-                if (this.dirty) {
-                    this.vertexArray.apply(gl);
-                    this.dirty = false;
-                }
+                this.vertexArray.apply(gl);
                 if (this.recolor) {
                     gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
                     this.recolor = false;
--- lib/webdjs/webdjs.ts
+++ lib/webdjs/webdjs.ts
@@ -3,47 +3,70 @@ module WebDJS {
 	    /**
 	     * Generic OpenGL operation.
 	     */
-        export interface GLOp {
-	        apply(gl : WebGLRenderingContext) : void;
+        export interface GLOperation {
+            apply(gl : WebGLRenderingContext) : void;
+        }
+        
+        /**
+         * Generic OpenGL texture operation.
+         */
+        export interface GLTextureOperation extends GLOperation {
+            bind(texture : WebGLTexture) : void;
         }
 		
 		/**
 		 * HTML Image -> Texture operation.
 		 */
-        export class HTMLImageTexOp implements GLOp {
+        export class GLImageInput implements GLTextureOperation {
 	        private source : HTMLImageElement;
 	        private texture : WebGLTexture;
+	        private loaded : boolean;
 	        constructor(source : HTMLImageElement, texture : WebGLTexture = null) { 
 	            this.source = source; 
 	            this.bind(texture);
 	        }
 	        bind(texture : WebGLTexture) : void {
 	            this.texture = texture;
+	            this.reload();
+	        }
+	        reload() : void {
+	            this.loaded = false;
 	        }
 	        apply(gl : WebGLRenderingContext) : void {
 	            gl.activeTexture(gl.TEXTURE0);
 	            gl.bindTexture(gl.TEXTURE_2D, this.texture);
-		        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+	            if (!this.loaded) {
+    		        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+    		        this.loaded = true;
+    		    }
 	        }
         }
 		
 		/**
 		 * HTML Video -> Texture operation.
 		 */
-	    export class HTMLVideoElementTexOp implements GLOp {
+	    export class GLVideoInput implements GLTextureOperation {
 		    private source : HTMLVideoElement;
 		    private texture : WebGLTexture;
+		    private loaded : boolean;
 		    constructor(source : HTMLVideoElement, texture : WebGLTexture = null) { 
 		        this.source = source; 
 		        this.bind(texture);
 		    }
 		    bind(texture : WebGLTexture) : void {
 		        this.texture = texture;
+		        this.reload();
+		    }
+		    reload() : void {
+		        this.loaded = false;
 		    }
 		    apply(gl : WebGLRenderingContext) : void {
 		        gl.activeTexture(gl.TEXTURE0);
 		        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-			    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+		        if (!this.loaded) {
+    			    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.source);
+    			    this.loaded = true;
+    			}
 		    }
 	    }
 		
@@ -52,7 +75,7 @@ module WebDJS {
 		 *
 		 * Transforms <x,y,width,height>-Tuples to a pair of Vertex/Index Buffers.
 		 */
-	    export class RectangularVertexArrayOp implements GLOp {
+	    export class GLRectangleVertexArray implements GLOperation {
 		    private width : number;
 		    private height : number;
 		    private x : number;
@@ -61,6 +84,8 @@ module WebDJS {
 		    private indices : Uint16Array = new Uint16Array([0,1,2,0,2,3]);
 		    private vertexBuffer : WebGLBuffer;
 		    private indexBuffer : WebGLBuffer;
+		    private changed : boolean;
+		    private loaded : boolean = false;
 		    constructor(vertexBuffer : WebGLBuffer = null, indexBuffer : WebGLBuffer = null) {
 		        this.bind(vertexBuffer, indexBuffer);
 		        this.translate(0, 0);
@@ -69,6 +94,8 @@ module WebDJS {
 		    bind(vertexBuffer : WebGLBuffer, indexBuffer : WebGLBuffer) : void {
 		        this.vertexBuffer = vertexBuffer;
 		        this.indexBuffer = indexBuffer;
+		        this.changed = true;
+		        this.loaded = false;
 		    }
 		    translate(x : number, y : number) : void {
 			    this.x = x;
@@ -77,6 +104,7 @@ module WebDJS {
 			    this.vertices[1] = this.y;
 			    this.vertices[3] = this.y;
 			    this.vertices[6] = this.x;
+			    this.changed = true;
 		    }
 		    resize(width : number, height : number) : void {
 			    this.width = width;
@@ -85,108 +113,53 @@ module WebDJS {
 			    this.vertices[4] = this.x + this.width;
 			    this.vertices[5] = this.y + this.height;
 			    this.vertices[7] = this.y + this.height;
+			    this.changed = true;
 		    }
 		    apply(gl : WebGLRenderingContext) : void {
 			    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-			    gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+			    if (this.changed) {
+    			    gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+    			    this.changed = false;
+    			}
 			    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-			    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+			    if (!this.loaded) {
+    			    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+    			    this.loaded = true;
+    			}
 		    }
 	    }
 		
 		/**
 		 * Simple Renderer - Render HTML Image/Video to GL context.
 		 */
-        export class Simple implements GLOp {
-            private texOp : GLOp;
+        export class Simple implements GLOperation {
+            private texOp : GLTextureOperation;
             private vertexShader : WebGLShader;
             private fragmentShader : WebGLShader;
             private shaderProgram : WebGLProgram;
-            private texture : WebGLTexture;
+            private glTexture : WebGLTexture;
             private xyAttribLocation : number;
             private samplerAttribLocation : WebGLUniformLocation;
             private rgbaAttribLocation : WebGLUniformLocation;
             private vertexBuffer : WebGLBuffer;
             private indexBuffer : WebGLBuffer;
-            private vertexArray : RectangularVertexArrayOp;
+            private vertexArray : GLRectangleVertexArray = new GLRectangleVertexArray();
             private rgba : Float32Array = new Float32Array([1,1,1,1]);
             private initialized : boolean = false;
-            private dirty : boolean = true;
             private recolor : boolean = true;
-            image(img : HTMLImageElement) : void {
-                if (!this.initialized) {
-                    return;
-                }
-                this.texOp = new HTMLImageTexOp(img, this.texture);
+            private rebind : boolean = false;
+            constructor(texOp : GLTextureOperation = null) {
+                this.texture(texOp);
             }
-            video(vid : HTMLVideoElement) : void {
-                if (!this.initialized) {
-                    return;
-                }
-                this.texOp = new HTMLVideoElementTexOp(vid, this.texture);
-            }
-            initialize(gl : WebGLRenderingContext) : void {
-                if (this.initialized) {
-                    return;
-                }
-                
-                this.vertexShader = gl.createShader(gl.VERTEX_SHADER);
-                gl.shaderSource(this.vertexShader, 
-                    "attribute vec2 vxy;" +
-                    "varying vec2 txy;" +
-                    "void main() {" +
-                    "   gl_Position = vec4(vxy.x*2.0-1.0, 1.0-vxy.y*2.0, 0, 1);" +
-                    "   txy = vxy;" +
-                    "}");
-                gl.compileShader(this.vertexShader);
-
-                this.fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-                gl.shaderSource(this.fragmentShader,
-                    "precision mediump float;" +
-                    "uniform sampler2D sampler;" +
-                    "uniform vec4 rgba;" +
-                    "varying vec2 txy;" +
-                    "void main() {" +
-                    "   vec4 texColor = texture2D(sampler, txy);" +
-                    "   gl_FragColor = texColor * rgba;" +
-                    "}");
-                gl.compileShader(this.fragmentShader);
-
-                this.shaderProgram = gl.createProgram();
-                gl.attachShader(this.shaderProgram, this.vertexShader);
-                gl.attachShader(this.shaderProgram, this.fragmentShader);
-                gl.linkProgram(this.shaderProgram);
-                gl.useProgram(this.shaderProgram);
-
-                this.xyAttribLocation = gl.getAttribLocation(this.shaderProgram, "vxy");
-                gl.enableVertexAttribArray(this.xyAttribLocation);
-                
-                this.samplerAttribLocation = gl.getUniformLocation(this.shaderProgram, "sampler");
-                gl.uniform1i(this.samplerAttribLocation, 0);
-                
-                this.rgbaAttribLocation = gl.getUniformLocation(this.shaderProgram, "rgba");
-                gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
-                
-                this.vertexBuffer = gl.createBuffer();
-                this.indexBuffer = gl.createBuffer();
-                this.vertexArray = new RectangularVertexArrayOp(this.vertexBuffer, this.indexBuffer);
-                
-                this.texture = gl.createTexture();
-                gl.bindTexture(gl.TEXTURE_2D, this.texture);        
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                
-                this.initialized = true;
+            texture(texOp : GLTextureOperation) : void {
+                this.texOp = texOp;
+                this.rebind = true;
             }
             translate(x : number, y : number) : void {
                 this.vertexArray.translate(x, y);
-                this.dirty = true;
             }
             resize(width : number, height : number) : void {
                 this.vertexArray.resize(width, height);
-                this.dirty = true;
             }
             color(r : number, g : number, b : number, a : number) : void {
                 this.rgba[0] = r;
@@ -197,17 +170,66 @@ module WebDJS {
             }
             apply(gl : WebGLRenderingContext) : void {
                 if (!this.initialized) {
-                    return;
+                    this.vertexShader = gl.createShader(gl.VERTEX_SHADER);
+                    gl.shaderSource(this.vertexShader, 
+                        "attribute vec2 vxy;" +
+                        "varying vec2 txy;" +
+                        "void main() {" +
+                        "   gl_Position = vec4(vxy.x*2.0-1.0, 1.0-vxy.y*2.0, 0, 1);" +
+                        "   txy = vxy;" +
+                        "}");
+                    gl.compileShader(this.vertexShader);
+
+                    this.fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+                    gl.shaderSource(this.fragmentShader,
+                        "precision mediump float;" +
+                        "uniform sampler2D sampler;" +
+                        "uniform vec4 rgba;" +
+                        "varying vec2 txy;" +
+                        "void main() {" +
+                        "   vec4 texColor = texture2D(sampler, txy);" +
+                        "   gl_FragColor = texColor * rgba;" +
+                        "}");
+                    gl.compileShader(this.fragmentShader);
+
+                    this.shaderProgram = gl.createProgram();
+                    gl.attachShader(this.shaderProgram, this.vertexShader);
+                    gl.attachShader(this.shaderProgram, this.fragmentShader);
+                    gl.linkProgram(this.shaderProgram);
+                    gl.useProgram(this.shaderProgram);
+
+                    this.xyAttribLocation = gl.getAttribLocation(this.shaderProgram, "vxy");
+                    gl.enableVertexAttribArray(this.xyAttribLocation);
+                    
+                    this.samplerAttribLocation = gl.getUniformLocation(this.shaderProgram, "sampler");
+                    gl.uniform1i(this.samplerAttribLocation, 0);
+                    
+                    this.rgbaAttribLocation = gl.getUniformLocation(this.shaderProgram, "rgba");
+                    gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
+                    
+                    this.vertexBuffer = gl.createBuffer();
+                    this.indexBuffer = gl.createBuffer();
+                    this.vertexArray.bind(this.vertexBuffer, this.indexBuffer);
+                    
+                    this.glTexture = gl.createTexture();
+                    gl.bindTexture(gl.TEXTURE_2D, this.glTexture);        
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                    
+                    this.initialized = true;
+                }
+                
+                if (this.rebind) {
+                    this.texOp.bind(this.glTexture);
+                    this.rebind = false;
                 }
                 
                 gl.useProgram(this.shaderProgram);
                 
                 this.texOp.apply(gl);
-                
-                if (this.dirty) {
-                    this.vertexArray.apply(gl);
-                    this.dirty = false;
-                }
+                this.vertexArray.apply(gl);
                 
                 if (this.recolor) {
                     gl.uniform4fv(this.rgbaAttribLocation, this.rgba);
